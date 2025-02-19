3d26309611159fdfd403a524f6500efe09084eab
tebanduarte2 <jeusugad@eafit.edu.co>
2025-02-18 20:38:25 -0500

    TutorialCartSystem

--- helloworld_project/pages/urls.py
+++ helloworld_project/pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path 
-from .views import homePageView, AboutPageView, ProductIndexView, ProductShowView,  ProductCreateView,HomePageView
+from .views import homePageView, AboutPageView, ProductIndexView, ProductShowView,  ProductCreateView,HomePageView, CartView, CartRemoveAllView
    
  
 urlpatterns = [ 
@@ -8,5 +8,7 @@ urlpatterns = [
     path('products/', ProductIndexView.as_view(), name='index'), 
     path('products/create', ProductCreateView.as_view(), name='form'),
     path('products/<str:id>', ProductShowView.as_view(), name='show'), 
-     
+    path('cart/', CartView.as_view(), name='cart_index'), 
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'), 
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
 ]
